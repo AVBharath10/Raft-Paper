0cfce31cc66a9abe700c98fd66cc5ed3fe7a8f89
AVBharath10 <avbharath1221@gmail.com>
2026-02-10 16:18:40 +0530

    feat: expose append entries handler from lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,7 @@
-pub mod state;
 pub mod node;
+pub mod state;
+
+pub use node::{
+    AppendEntriesArgs, AppendEntriesReply, LogEntry, Node, RequestVoteArgs, RequestVoteReply,
+};
+pub use state::State;
