6329807b92127be8598b408f390734632ca8df2f
AVBharath10 <avbharath1221@gmail.com>
2026-02-10 16:19:38 +0530

    fix: incorrect state transition from candidate to follower

--- src/state.rs
+++ src/state.rs
@@ -1,6 +1,16 @@
-#[derive(Debug)]
-pub enum State{
+#[derive(Debug, Clone, PartialEq)]
+pub enum State {
     Leader,
     Follower,
-    Candidate
+    Candidate,
+}
+
+impl std::fmt::Display for State {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            State::Leader => write!(f, "Leader"),
+            State::Follower => write!(f, "Follower"),
+            State::Candidate => write!(f, "Candidate"),
+        }
+    }
 }
